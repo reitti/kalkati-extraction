19797a9f6979f7ffaf32579136fcc6834badf538
Tero Parviainen <tero.parviainen@eficode.fi>
2012-10-06 16:24:01 +0300

    read XML and write to index files given as command line arguments

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject kalkati-extraction "0.1.0-SNAPSHOT"
   :repositories [["sonatype" "https://oss.sonatype.org/content/groups/public/"]]
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
-                 [clj-http "0.5.5"]]
+                 [clj-http "0.5.5"]
+                 [org.clojure/data.xml "0.0.6"]]
   :main kalkati.core)
--- src/kalkati/core.clj
+++ src/kalkati/core.clj
@@ -1,22 +1,65 @@
 (ns kalkati.core
-  (:require [clj-http.client :as http]
-            [clojure.java.io :refer [reader]])
+  (:require [clojure.string :refer [split]]
+            [clojure.java.io :refer [writer]]
+            [clj-http.client :as http]
+            [clojure.data.xml :refer :all])
+  (:import [java.io PrintWriter])
   (:gen-class))
 
 (def hsl-username (or (System/getenv "HSL_API_USERNAME") (System/getProperty "hsl.api.username")))
 (def hsl-password (or (System/getenv "HSL_API_PASSWORD") (System/getProperty "hsl.api.password")))
 
+(def cities
+  {"1" "Helsinki"
+   "2" "Espoo"
+   "3" "Kauniainen"
+   "4" "Vantaa"
+   "6" "Kirkkonummi"
+   "9" "Kerava"})
+
+(defn- city-writers [args]
+  (->> args
+       (map
+         (fn [arg]
+           (let [[city filename] (split arg #"=")]
+             [city (PrintWriter. (writer filename :append true) true)])))
+       (into {})))
+
+(defn- close-city-writers [writers]
+  (doseq [writer (vals writers)]
+    (.close writer)))
+
+(defmacro with-open-city-writers [[writers args] & body]
+  `(let [~writers (city-writers ~args)]
+     (try
+       ~@body
+       (finally (close-city-writers ~writers)))))
+
 (defn- kalkati-data []
   (let [in (-> "http://api.reittiopas.fi/data/all.zip"
                 (http/get {:basic-auth [hsl-username hsl-password]
                            :as :stream})
-                :body
-                java.util.zip.ZipInputStream.)]
-    (.getNextEntry in)
-    (reader in)))
+                :body)]
+    (doto (java.util.zip.ZipInputStream. in)
+      (.getNextEntry))))
+
+(defn- stations [stream]
+  (->> stream
+       parse
+       :content
+       (filter #(= :Station (:tag %)))
+       (map :attrs)))
 
 (defn -main [& args]
   (with-open [stream (kalkati-data)]
-    (doseq [line (line-seq stream)]
-      (println line))))
+    (with-open-city-writers [writers args]
+      (doseq [{:keys [Name X Y city_id]} (stations stream)]
+        (when-let [^PrintWriter writer (writers (cities city_id))]
+          (doto writer
+            (.print Name)
+            (.print "|")
+            (.print X)
+            (.print ",")
+            (.print Y)
+            (.println)))))))
 
