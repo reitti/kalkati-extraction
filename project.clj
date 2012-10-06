85bf6414bb451798ecac2d2b1e6cdc0a27949d0c
Tero Parviainen <tero.parviainen@eficode.fi>
2012-10-06 15:14:45 +0300

    reading in of Kalkati XML dump

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject kalkati-extraction "0.1.0-SNAPSHOT"
   :repositories [["sonatype" "https://oss.sonatype.org/content/groups/public/"]]
-  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
+                 [clj-http "0.5.5"]]
   :main kalkati.core)
--- src/kalkati/core.clj
+++ src/kalkati/core.clj
@@ -1,6 +1,22 @@
 (ns kalkati.core
+  (:require [clj-http.client :as http]
+            [clojure.java.io :refer [reader]])
   (:gen-class))
 
-(defn -main
-  [& args]
-  (println "Hello, World!"))
+(def hsl-username (or (System/getenv "HSL_API_USERNAME") (System/getProperty "hsl.api.username")))
+(def hsl-password (or (System/getenv "HSL_API_PASSWORD") (System/getProperty "hsl.api.password")))
+
+(defn- kalkati-data []
+  (let [in (-> "http://api.reittiopas.fi/data/all.zip"
+                (http/get {:basic-auth [hsl-username hsl-password]
+                           :as :stream})
+                :body
+                java.util.zip.ZipInputStream.)]
+    (.getNextEntry in)
+    (reader in)))
+
+(defn -main [& args]
+  (with-open [stream (kalkati-data)]
+    (doseq [line (line-seq stream)]
+      (println line))))
+
